edbac73ad3b04b4fecee50f5da91928465ab670d
anishsharma21 <anisharma2103@gmail.com>
2025-02-18 18:10:36 +1100

    CICD + Testing - added test for login handler, removed -v flag for tests in cicd pipeline, other logs are outputted and aren't necessary to be viewed unless a test fails, looks better without -v flag too.

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -2,8 +2,10 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -11,6 +13,7 @@ import (
 
 	"github.com/anishsharma21/go-web-dev-template/internal/handlers"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // dbPool is the database connection pool used for the tests that require database interaction
@@ -71,6 +74,53 @@ func TestUserSignUpFlow(t *testing.T) {
 	// Verify
 	if resp.StatusCode != 200 {
 		t.Errorf("Expected status code 200, got %v\n", resp.StatusCode)
+	}
+
+	// Tear down
+	_, err = dbPool.Exec(ctx, "DELETE FROM users WHERE email = $1", email)
+	if err != nil {
+		t.Fatalf("Failed to delete user from database, %v\n", err)
+	}
+}
+
+func TestUserLoginFlow(t *testing.T) {
+	// Prepare
+	email := "testperson1@gmail.com"
+	password := "password1"
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	// Insert test user into the database
+	_, err := dbPool.Exec(ctx, "INSERT INTO users (email, password) VALUES ($1, $2)", email, string(hashedPassword))
+	if err != nil {
+		t.Fatalf("Failed to insert test user into database, %v\n", err)
+	}
+
+	ts := httptest.NewServer(handlers.Login(dbPool))
+	defer ts.Close()
+
+	// Execute
+	resp, err := ts.Client().PostForm(ts.URL, map[string][]string{
+		"email":    {email},
+		"password": {password},
+	})
+	if err != nil {
+		t.Fatalf("Expected no error when sending POST request to /login, got %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	// Verify
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code 200, got %v\n", resp.StatusCode)
+	}
+
+	var response map[string]string
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		t.Fatalf("Expected no error when decoding response, got %v\n", err)
+	}
+
+	if _, ok := response["token"]; !ok {
+		t.Errorf("Expected token in response, got %v\n", response)
 	}
 
 	// Tear down
